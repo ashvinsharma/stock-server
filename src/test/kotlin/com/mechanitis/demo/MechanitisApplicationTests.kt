f25f4dab34883576a1664a82776feaa616086b99
ashvinsharma97@gmail.com <lnmkcj1e>
2020-05-08 03:58:44 +0530

    first

---
+++ src/test/kotlin/com/mechanitis/demo/MechanitisApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mechanitis.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MechanitisApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
